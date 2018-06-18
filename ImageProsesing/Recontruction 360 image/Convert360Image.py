a00173ccc7ddc762cb7fd263926a26bbfc1bf0a4
RafaSarmi <rasarmientos@unal.edu.co>
2018-06-18 09:01:24 -0500

    def
    def

--- ImageProsesing/Recontruction 360 image/Convert360Image.py
+++ ImageProsesing/Recontruction 360 image/Convert360Image.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 import time
 
-def buildMap(Ws,Hs,Wd,Hd,R1,R2,Cx,Cy):
+def mapCirToRect(Ws,Hs,Wd,Hd,R1,R2,Cx,Cy):
     map_x = np.zeros((Hd,Wd),np.float32)
     map_y = np.zeros((Hd,Wd),np.float32)
     for y in range(0,(Hd-1)):
@@ -15,7 +15,7 @@ def buildMap(Ws,Hs,Wd,Hd,R1,R2,Cx,Cy):
             map_y.itemset((y,x),int(yS))
     return map_x, map_y
 
-def unwarp(img,xmap,ymap):
+def remapImage(img,xmap,ymap):
     result = cv2.remap(img,xmap,ymap,cv2.INTER_LINEAR)
     return result
 
@@ -39,13 +39,13 @@ Wd = int(round(float(R2) * 2.0 * np.pi))
 Hd = int((R2-R1))
 
 print("MAPEANDO")
-xmap,ymap = buildMap(Ws,Hs,Wd,Hd*2,R1,R2,Cx,Cy)
+xmap,ymap = mapCirToRect(Ws,Hs,Wd,Hd*2,R1,R2,Cx,Cy)
 print("ok")
 
 while(cap.isOpened()):
     ret, img = cap.read()
     if ret == True:
-        result = unwarp(img,xmap,ymap)
+        result = remapImage(img,xmap,ymap)
         cv2.imshow('Correction',result)
         cv2.circle(img,(cmaxx,cmaxy),max,(0,255,0),2)
         cv2.circle(img,(cmaxx,cmaxy),2,(0,0,255),3)
